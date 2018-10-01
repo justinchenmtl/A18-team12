92dbf40b3bdddb8cb2132f493f8860acaa5dddd1
Fadi <fadi.feghaly@gmail.com>
2018-09-30 20:15:13 -0400

    v1.1 experimental

--- Terrain.java
+++ Terrain.java
@@ -1,13 +1,12 @@
-import java.util.ArrayList;
+
+import net.sf.json.JSONArray;
+
 /**
- * Utiliser pour decrire un terrain qui sera lu a partir d'un fichier
- * JSON.
+ * Utiliser pour decrire un terrain qui sera lu a partir d'un fichier JSON.
  *
- * Un terrain a 4 attributs:
- *  - un type (en general, le type peut etre n'importe quel entier, depend de la situation externe)
- *  - un prix minimum par metre carre
- *  - un prix maximum par metre carre
- *  - un ou plusieurs lots
+ * Un terrain a 4 attributs: - un type (en general, le type peut etre n'importe
+ * quel entier, depend de la situation externe) - un prix minimum par metre
+ * carre - un prix maximum par metre carre - un ou plusieurs lots
  *
  * @author josue
  * @version 1.0
@@ -19,22 +18,24 @@ public class Terrain {
     // **********
     public static final int AGRICOLE = 0;
     public static final int RESIDENTIEL = 1;
-    public static  final int COMMERCIAL = 2;
+    public static final int COMMERCIAL = 2;
 
     // On peut peut-etre les rendre public pour ne pas ecrire les setter et getter
     public int type;
     public double prixMin;
+    public double prixMoyen;
     public double prixMax;
-    public ArrayList<Lot> lotissement; // Peut etre changer a ArrayList pour faciler l'ajout de lots
+    public JSONArray lotissement; // Peut etre changer a ArrayList pour faciler l'ajout de lots
 
-    /**
-     * Contructeur par default. Initialise tout les attribut
-     * a leur valeur par defaut. (0 pour int et null pour String)
+    /*
+     * Contructeur par default. Initialise tout les attribut a leur valeur par
+     * defaut. (0 pour int et null pour String)
      */
-    public Terrain(){
+    public Terrain() {
 
         type = 0;
         prixMin = 0;
+        prixMoyen = 0;
         prixMax = 0;
         lotissement = null;
 
@@ -42,59 +43,34 @@ public class Terrain {
 
     /**
      *
-     * @param type          le type de terrain
-     * @param prixMin       le prix minimum par metre carre
-     * @param prixMax       le prix maximum par metre carre
-     * @param lotissement   les lots dans ce terrain
+     * @param type le type de terrain
+     * @param prixMin le prix minimum par metre carre
+     * @param prixMax le prix maximum par metre carre
+     * @param lotissement les lots dans ce terrain
      *
-     * @throws TerrainException si le prix minimum et le prix maximum sont inferieur ou egale a 0
+     * @throws TerrainException si le prix minimum et le prix maximum sont
+     * inferieur ou egale a 0
      */
-    public Terrain(int type, double prixMin, double prixMax, ArrayList<Lot> lotissement)
-    throws TerrainException{
+    public Terrain(int type, double prixMin, double prixMax, JSONArray lotissement)
+            throws TerrainException {
         // Ce n'est pas necessairement la meilleur maniere de valider les donnes.
         // Possiblement a changer
-        if(prixMin > 0 && prixMax > 0 && null != lotissement) {
+        if (prixMin > 0 && prixMax > 0 && null != lotissement) {
             this.type = type;
             this.prixMin = prixMin;
+            this.prixMoyen = (prixMin + prixMax) / 2;
             this.prixMax = prixMax;
             this.lotissement = lotissement;
         } else {
             // A modifier pour plus de precision
-            throw new TerrainException("Terrain invalide.");
+            throw new TerrainException("Terrain non conforme.");
         }
 
-
     }
 
-    public void setType(int type) {
-        this.type = type;
+    public double getPrixMoyen() {
+        return prixMoyen;
     }
 
-    public void setLotissement(ArrayList<Lot> lotissement) {
-        this.lotissement = lotissement;
-    }
 
-    public void setPrixMax(double prixMax) {
-        this.prixMax = prixMax;
-    }
-
-    public void setPrixMin(double prixMin) {
-        this.prixMin = prixMin;
-    }
-
-    public int getType() {
-        return type;
-    }
-
-    public ArrayList<Lot> getLotissement() {
-        return lotissement;
-    }
-
-    public double getPrixMax() {
-        return prixMax;
-    }
-
-    public double getPrixMin() {
-        return prixMin;
-    }
 }
