f758518c428fbf1052966be452eb0cecf08c5d49
fadifeghaly <43209786+fadifeghaly@users.noreply.github.com>
2018-10-02 14:19:58 -0400

    Revert "v1.1 experimental"

--- Principale.java
+++ Principale.java
@@ -1,9 +1,7 @@
-
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import net.sf.json.JSONObject;
 import net.sf.json.JSONArray;
+
 
 /**
  *
@@ -11,6 +9,7 @@ import net.sf.json.JSONArray;
  */
 // On devrait changer le nom de cette classe pour un nom plus significatif
 public class Principale {
+
 
     // **********
     // CONSTANTES
@@ -27,67 +26,136 @@ public class Principale {
     final static double POURCENTAGE_DROIT_PASSAGE_COMMERCIAL = 0.15;
     final static double PLAFOND_VALEUR_SERVICE_COMMERCIAL = 5000;
 
+    static double valeurLot(){
+        return 0;
+    }
+
+    static double valeurLotAgricole(){
+        return 0;
+    }
+
+    static double valeurLotResidentiel(){
+        return 0;
+    }
+
+    static double valeurLotCommercial(){
+        return 0;
+    }
+
+    static double valeurDroitPass(){
+        return 0;
+    }
+
+    static double valeurDroitPassAgricole(){
+        return 0;
+    }
+
+    static double valeurDroitPassResidentiel(){
+        return 0;
+    }
+
+    static double valeurDroitPassCommercial(){
+        return 0;
+    }
+
+    static double valeurService(){
+        return 0;
+    }
+
+    static double valeurServiceAgricole(){
+        return 0;
+    }
+
+    static double valeurServiceResidentiel(){
+        return 0;
+    }
+
+    static double valeurServiceCommercial(){
+        return 0;
+    }
+
+    static double valeurFonciereTotale(){
+        return 0;
+    }
+
     /**
      * @param args the command line arguments
      * @throws java.io.IOException
      */
-    public static void main(String[] args) throws IOException, TerrainException, LotException {
+    public static void main(String[] args) throws IOException {
         //Déclaration des variables et objets
-        JSONObject mainObject;
+        String terrainJSON;
+        JSONObject terrain;
         JSONArray lotArray;
         JSONObject lotObject;
         int typeTerrain;
-        double prixMin;
-        double prixMax;
-        double valeurFonciereTotale = 0.0;
-        double valeurParLot;
+        Double prixMin;
+        Double prixMax;
         String lotDescription;
         int nombreDroit;
         int nombreService;
         int superficie;
-        String date_mesure;
+        Double valeurFonciereTotale = 0.0;
         JSONArray lotSortieArray = new JSONArray();
         JSONObject lotSortieObject = new JSONObject();
-
+        
         //lire le fichier d'entrée, en format JSON
-        String json = FileReader.loadFileIntoString(FILEPATH_INPUT, CHARSET_ENCODING);
-        mainObject = JSONObject.fromObject(json);
-        typeTerrain = mainObject.getInt("type_terrain");
-        prixMin = mainObject.getDouble("prix_m2_min");
-        prixMax = mainObject.getDouble("prix_m2_max");
-        lotArray = mainObject.getJSONArray("lotissements");
-        Terrain t = new Terrain(typeTerrain, prixMin, prixMax, lotArray);
-        for (int i = 0; i < lotArray.size(); ++i) {
-            lotObject = lotArray.getJSONObject(i);
-            lotDescription = lotObject.getString("description");
-            nombreDroit = lotObject.getInt("nombre_droits_passage");
-            nombreService = lotObject.getInt("nombre_services");
-            superficie = lotObject.getInt("superficie");
-            date_mesure = lotObject.getString("date_mesure");
-            Lot lot = new Lot(lotDescription, nombreDroit, nombreService, superficie, date_mesure);
-            double valeur_Lot = lot.ValeurLot(typeTerrain, superficie, prixMin, prixMax, t.getPrixMoyen());
-            double valeur_DroitPassage = lot.ValeurDroitPassage(typeTerrain, nombreDroit, valeur_Lot);
-            double valeur_services = lot.ValeurServices(typeTerrain, nombreService, superficie);
-            valeurParLot = VALEUR_FONCIERE_FIXE + valeur_Lot + valeur_DroitPassage + valeur_services;
-            valeurFonciereTotale += valeurParLot;
-            lotSortieObject.clear();
-            lotSortieObject.accumulate("description", lotDescription);
-            lotSortieObject.accumulate("valeur_par_lot", valeurParLot);
-            lotSortieArray.add(lotSortieObject);
+        terrainJSON = FileReader.loadFileIntoString(FILEPATH_INPUT, CHARSET_ENCODING);
+        terrain = JSONObject.fromObject(terrainJSON);
+        typeTerrain = terrain.getInt("type_terrain");
+        prixMin = terrain.getDouble("prix_m2_min");
+        prixMax = terrain.getDouble("prix_m2_max");
+        
+        //obtenir l'arraylist dans le fichier d'entrée
+        lotArray = terrain.getJSONArray("lotissements");
+        if(typeTerrain == 2){//2 représente le type de terrain commercial
+            for(int i = 0; i < lotArray.size(); ++i){
+                lotObject = lotArray.getJSONObject(i);
+                lotDescription = lotObject.getString("description");
+                nombreDroit = lotObject.getInt("nombre_droits_passage");
+                nombreService = lotObject.getInt("nombre_services");
+                superficie = lotObject.getInt("superficie");
+                
+                //calcul de la valeur foncière du lot
+                Double montantValeurLot = 0.0;
+                Double montantDroitPassage = 0.0;
+                Double montantService = 0.0;
+                Double valeurParLot = 0.0; 
+                montantValeurLot = superficie * prixMax;
+                montantDroitPassage = 500-(nombreDroit*(0.15*montantValeurLot));//500 représent un montant de base                
+                if(superficie <=500){
+                    montantService = 500.00;
+                }else{
+                    montantService = 1500.00*(nombreService + 2);//1500.00 $ du service, si la superficie est supérieure à 500 m2
+                    if(montantService > 5000.00){
+                        montantService = 5000.00;//Le montant pour les services ne peut pas dépasser les 5000.00 $
+                    }
+                }
+                //Un montant fixe de 733.77 $ est ajouté à la valeur foncière pour couvrir la valeur de base
+                valeurParLot = 733.77 + montantValeurLot + montantDroitPassage + montantService;
+                valeurFonciereTotale += valeurParLot; 
+                
+                //générer l'arraylist dans le fichier sortie, en format JSON
+                lotSortieObject.clear();
+                lotSortieObject.accumulate("description", lotDescription);
+                lotSortieObject.accumulate("valeur_par_lot", valeurParLot);
+                lotSortieArray.add(lotSortieObject);                
+            }
+            
+            //calcul de la valeur foncière du terrain tatale et générer les objets dans le fichier sortie
+            Double taxeScolaire = valeurFonciereTotale * 0.012; //La taxe scolaire représente 1.2% de la valeur foncière totale
+            Double taxeMunicipale = valeurFonciereTotale * 0.025; //la taxe municipale représente 2.5 % de la valeur foncière totale
+            JSONObject terrainSortieJSON = new JSONObject();
+            terrainSortieJSON.accumulate("valeur_fonciere_totale", valeurFonciereTotale);
+            terrainSortieJSON.accumulate("taxe_scolaire", taxeScolaire);
+            terrainSortieJSON.accumulate("taxe_ municipale", taxeMunicipale);
+            terrainSortieJSON.accumulate("lotissements", lotSortieArray);
+            
+            //créer le fichier sortie, en format JSON
+            FileWriter.saveStringIntoFile(FILEPATH_OUTPUT, terrainSortieJSON.toString());
+            
         }
-
-        Double taxeScolaire = valeurFonciereTotale * 0.012; //La taxe scolaire représente 1.2% de la valeur foncière totale
-        Double taxeMunicipale = valeurFonciereTotale * 0.025; //la taxe municipale représente 2.5 % de la valeur foncière totale
-        JSONObject terrainSortieJSON = new JSONObject();
-        terrainSortieJSON.accumulate("valeur_fonciere_totale", valeurFonciereTotale);
-        terrainSortieJSON.accumulate("taxe_scolaire", taxeScolaire);
-        terrainSortieJSON.accumulate("taxe_ municipale", taxeMunicipale);
-        terrainSortieJSON.accumulate("lotissements", lotSortieArray);
-
-        //créer le fichier sortie, en format JSON
-        FileWriter.saveStringIntoFile(FILEPATH_OUTPUT, terrainSortieJSON.toString());
-
-        //créer le fichier sortie, en format JSON
-        //FileWriter.saveStringIntoFile(FILEPATH_OUTPUT, terrainSortieJSON.toString());
     }
+    
 }
+
--- Terrain.java
+++ Terrain.java
@@ -1,12 +1,13 @@
-
-import net.sf.json.JSONArray;
-
+import java.util.ArrayList;
 /**
- * Utiliser pour decrire un terrain qui sera lu a partir d'un fichier JSON.
+ * Utiliser pour decrire un terrain qui sera lu a partir d'un fichier
+ * JSON.
  *
- * Un terrain a 4 attributs: - un type (en general, le type peut etre n'importe
- * quel entier, depend de la situation externe) - un prix minimum par metre
- * carre - un prix maximum par metre carre - un ou plusieurs lots
+ * Un terrain a 4 attributs:
+ *  - un type (en general, le type peut etre n'importe quel entier, depend de la situation externe)
+ *  - un prix minimum par metre carre
+ *  - un prix maximum par metre carre
+ *  - un ou plusieurs lots
  *
  * @author josue
  * @version 1.0
@@ -18,24 +19,22 @@ public class Terrain {
     // **********
     public static final int AGRICOLE = 0;
     public static final int RESIDENTIEL = 1;
-    public static final int COMMERCIAL = 2;
+    public static  final int COMMERCIAL = 2;
 
     // On peut peut-etre les rendre public pour ne pas ecrire les setter et getter
     public int type;
     public double prixMin;
-    public double prixMoyen;
     public double prixMax;
-    public JSONArray lotissement; // Peut etre changer a ArrayList pour faciler l'ajout de lots
+    public ArrayList<Lot> lotissement; // Peut etre changer a ArrayList pour faciler l'ajout de lots
 
-    /*
-     * Contructeur par default. Initialise tout les attribut a leur valeur par
-     * defaut. (0 pour int et null pour String)
+    /**
+     * Contructeur par default. Initialise tout les attribut
+     * a leur valeur par defaut. (0 pour int et null pour String)
      */
-    public Terrain() {
+    public Terrain(){
 
         type = 0;
         prixMin = 0;
-        prixMoyen = 0;
         prixMax = 0;
         lotissement = null;
 
@@ -43,34 +42,59 @@ public class Terrain {
 
     /**
      *
-     * @param type le type de terrain
-     * @param prixMin le prix minimum par metre carre
-     * @param prixMax le prix maximum par metre carre
-     * @param lotissement les lots dans ce terrain
+     * @param type          le type de terrain
+     * @param prixMin       le prix minimum par metre carre
+     * @param prixMax       le prix maximum par metre carre
+     * @param lotissement   les lots dans ce terrain
      *
-     * @throws TerrainException si le prix minimum et le prix maximum sont
-     * inferieur ou egale a 0
+     * @throws TerrainException si le prix minimum et le prix maximum sont inferieur ou egale a 0
      */
-    public Terrain(int type, double prixMin, double prixMax, JSONArray lotissement)
-            throws TerrainException {
+    public Terrain(int type, double prixMin, double prixMax, ArrayList<Lot> lotissement)
+    throws TerrainException{
         // Ce n'est pas necessairement la meilleur maniere de valider les donnes.
         // Possiblement a changer
-        if (prixMin > 0 && prixMax > 0 && null != lotissement) {
+        if(prixMin > 0 && prixMax > 0 && null != lotissement) {
             this.type = type;
             this.prixMin = prixMin;
-            this.prixMoyen = (prixMin + prixMax) / 2;
             this.prixMax = prixMax;
             this.lotissement = lotissement;
         } else {
             // A modifier pour plus de precision
-            throw new TerrainException("Terrain non conforme.");
+            throw new TerrainException("Terrain invalide.");
         }
 
+
     }
 
-    public double getPrixMoyen() {
-        return prixMoyen;
+    public void setType(int type) {
+        this.type = type;
     }
 
+    public void setLotissement(ArrayList<Lot> lotissement) {
+        this.lotissement = lotissement;
+    }
 
+    public void setPrixMax(double prixMax) {
+        this.prixMax = prixMax;
+    }
+
+    public void setPrixMin(double prixMin) {
+        this.prixMin = prixMin;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public ArrayList<Lot> getLotissement() {
+        return lotissement;
+    }
+
+    public double getPrixMax() {
+        return prixMax;
+    }
+
+    public double getPrixMin() {
+        return prixMin;
+    }
 }
